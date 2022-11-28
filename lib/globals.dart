16037bf979ae40631b10cb333853f8bb4371cbd1
trueToastedCode <lennard2hp@gmail.com>
2022-11-28 14:55:11 +0100

    set always mobile

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,7 +1,8 @@
 library anyplist.globals;
 
-//import 'dart:io' show Platform;
-import 'package:flutter/foundation.dart';
+// import 'dart:io' show Platform;
+// import 'package:flutter/foundation.dart';
 
 // final isMobile = Platform.isAndroid || Platform.isIOS || Platform.isFuchsia;
-final isMobile = defaultTargetPlatform == TargetPlatform.iOS || defaultTargetPlatform == TargetPlatform.android || defaultTargetPlatform == TargetPlatform.fuchsia;
+// final isMobile = defaultTargetPlatform == TargetPlatform.iOS || defaultTargetPlatform == TargetPlatform.android || defaultTargetPlatform == TargetPlatform.fuchsia;
+const isMobile = true;
