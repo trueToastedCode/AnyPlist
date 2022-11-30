e26eb60ca15ba34118e069ea19c1a7688c5adfde
trueToastedCode <lennard2hp@gmail.com>
2022-11-30 11:48:16 +0100

    deactivate ads

--- lib/services/adblock_detection/adblock_detection_impl.dart
+++ lib/services/adblock_detection/adblock_detection_impl.dart
@@ -1,15 +1,16 @@
-import 'dart:js_util';
-import 'package:js/js.dart';
+// import 'dart:js_util';
+// import 'package:js/js.dart';
 
 import 'package:anyplist/services/adblock_detection/adblock_detection_contract.dart';
 
-@JS()
-external detectAdblock();
+// @JS()
+// external detectAdblock();
 
 class AdblockDetection implements IAdblockDetection {
   @override
   Future<bool> isBocked() async {
-    final promise = detectAdblock();
-    return await promiseToFuture(promise);
+    // final promise = detectAdblock();
+    // return await promiseToFuture(promise);
+    return false;
   }
 }
